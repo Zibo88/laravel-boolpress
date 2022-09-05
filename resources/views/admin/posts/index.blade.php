a375738699fd037f9af4f6f5f9eaaa4177485952
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-05 18:36:53 +0200

    ciclo foreach per leggere i dati, post disposti in 3 colonne con un margintop di 3

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,16 +1,19 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <div class="row ">
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="card-link">Card link</a>
-                  <a href="#" class="card-link">Another link</a>
+    <div class="row row-cols-3">
+        @foreach ($posts as $post)
+            <div class="col">
+                <div class="card mt-3">
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                    </div>
                 </div>
             </div>
-        </div>
+        @endforeach
+        
     </div>
 @endsection
