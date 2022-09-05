28af4f1460621b156b2bbb90bea53a05a8cdb546
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-05 19:39:42 +0200

    tag <a> collegamento attraverso id alla pagina show di quell'elemento

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -11,8 +11,9 @@
                     <div class="card-body">
                     <h5 class="card-title">{{$post->title}}</h5>
                     <p class="card-text">{{$post->content}}</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
+                    {{-- collegamento attraverso id alla pagina show di quell'elemento, il parametro richiesto è [parametro] --}}
+                    <a href="{{route ('admin.posts.show' , ['post' => $post->id])}}" class="card-link">Leggi di più</a>
+                    {{-- <a href="#" class="card-link">Another link</a> --}}
                     </div>
                 </div>
             </div>
