2cbe5680ec6f520e71079d3039d358a0c283b01f
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-05 20:14:15 +0200

    invio i dati a admin.posts.store tramite metodo post

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <form action="">
+    <form action=""{{route ('admin.posts.store')}} method="post">
+        {{-- sicurezza del form --}}
+        @csrf
         <div class="mb-3">
             <label for="title" class="form-label">Titolo</label>
             <input type="text" class="form-control" id="tile" name="title">
@@ -10,6 +12,8 @@
             <label for="content" class="form-label">Contenuto</label>
             <textarea class="form-control" id="content" name="content" rows="3"></textarea>
         </div>
+        {{-- bottone per salvare --}}
+        <input type="submit" value="Salva"> 
     </form>
     
 @endsection
