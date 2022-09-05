b42042c00206e0d339bb6038cad407976547b61f
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-05 19:16:25 +0200

    stampo i dati relativi alla creazione e modifica del post

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,5 +3,9 @@
 @section('content')
 {{-- stampo in pagina i dati provenienti da Postcontroller show() --}}
     <h1>{{$post->title}}</h1>
+    {{-- stampo i dati relativi alla creazione e modifica del post --}}
+    <div> Creato il:{{$post->created_at}}</div>
+    <div> Aggiornato il:{{$post->updated_at}}</div>
     <p>{{$post->content}}</p>
+
 @endsection
