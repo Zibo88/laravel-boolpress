0844bb17b563a7dd39f6bce2fd600f98e2088373
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-05 18:43:24 +0200

    stampati a monitor titolo e contenuto del post

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,11 +3,14 @@
 @section('content')
     <div class="row row-cols-3">
         @foreach ($posts as $post)
+            {{-- controllo come sono nominate le chiavi --}}
+            {{-- {{dd($post)}} --}}
+
             <div class="col">
                 <div class="card mt-3">
                     <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h5 class="card-title">{{$post->title}}</h5>
+                    <p class="card-text">{{$post->content}}</p>
                     <a href="#" class="card-link">Card link</a>
                     <a href="#" class="card-link">Another link</a>
                     </div>
