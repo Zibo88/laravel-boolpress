27b1047225a6d2d36dbdc934972b6cc25c559170
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-05 20:02:21 +0200

    aggiunto form a create.blade, modificati id/for e name

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <h1> qui ci sarà il form</h1>
+    <div class="mb-3">
+        <label for="title" class="form-label">Titolo</label>
+        <input type="text" class="form-control" id="tile" name="title">
+    </div>
+    <div class="mb-3">
+        <label for="content" class="form-label">Contenuto</label>
+        <textarea class="form-control" id="content" name="content" rows="3"></textarea>
+    </div>
 @endsection
