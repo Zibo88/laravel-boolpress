3a50b912ed6d1781945400c3a795f0c38ca5a402
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-06 14:40:01 +0200

    modificata route, e motodo di trasferimento dei dati

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <h1>{{dd($post)}}}}</h1>
+{{-- modifico la route in modo tale che trasferisca i dati raccolti dal form ad Update --}}
+<form action="{{route ('admin.posts.update' , ['post' => $post->id])}}" method="post">
+    @csrf
+    {{-- dato che update riceve i dati con il metodo PUT inserirò: --}}
+    @method('PUT')
+    <div class="mb-3">
+        <label for="title" class="form-label">Titolo</label>
+        {{-- aggiunta di old alla value --}}
+        <input type="text" class="form-control" id="title" name="title" >
+    </div>
+    <div class="mb-3">
+        <label for="content" class="form-label">Contenuto</label>
+        <textarea class="form-control" id="content" rows="3" name="content" value=""></textarea>
+    </div>
+
+    <input type="submit" value="Modifica">
+</form>
 @endsection
