ae305c63d4b687a5a1eadf4a61eb9e51b5002583
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-07 19:27:46 +0200

    creo la select in view create per dare la possibilità all'utente di modificarla

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -15,11 +15,26 @@
     <form action="{{route ('admin.posts.store')}}" method="post">
         @csrf
         @method('POST')
+        {{-- select --}}
+        {{-- creo la select in view create per dare la possibilità all'utente di modificarla --}}
+        <label for="category_id">Categoria</label>
+        <div class="mb-3">
+            <select class="form-select" aria-label="Default select example" name="category_id">
+                <option selected>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
+       
+
+        {{-- titolo --}}
         <div class="mb-3">
             <label for="title" class="form-label">Titolo</label>
             {{-- aggiunta di old alla value --}}
             <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}">
         </div>
+        {{-- contenuto --}}
         <div class="mb-3">
             <label for="content" class="form-label">Contenuto</label>
             <textarea class="form-control" id="content" rows="3" name="content" value="">{{old('content')}}</textarea>
