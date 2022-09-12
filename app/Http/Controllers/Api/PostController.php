c928baa13fbb2ae6668c766d35d5e36361571f29
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-12 16:22:55 +0200

    richiamo con il model tutti i post

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+// importo il model che contiene i dati del db
+use App\Post;
 
 class PostController extends Controller
 {
    public function index(){
-       (dd(index()));
+
+    // richiamo con il model tutti i post
+    $post = Post::all();
+    dd($post);
+    
+       
    }
 }
