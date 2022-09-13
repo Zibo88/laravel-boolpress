0c92f023fb980e84b0f08d587307994946cae003
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-13 18:55:31 +0200

    importo le pagine

--- resources/js/router.js
+++ resources/js/router.js
@@ -4,4 +4,11 @@ import VueRouter from 'vue-router';
 			
 Vue.use(VueRouter);
 
+// Regole delle rotte
+// importo le pagine
+import HomePage from './pages/HomePage.vue';
+import AboutPage from './pages/AboutPage.vue';
+import BlogPage from './pages/BlogPage.vue';
+
+
 
