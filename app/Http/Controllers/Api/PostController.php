f7a6e92cf38a77cbbe80a48534e8aac1ec589d55
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-14 18:14:19 +0200

    modificata da 'post' a 'results'

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -41,7 +41,7 @@ class PostController extends Controller
         if($post){
             $data = [
                 'success' => true,
-                'post' => $post
+                'results' => $post
             ];
         }else{
             $data = [
