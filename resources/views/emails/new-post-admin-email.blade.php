75b156f0749d3000ffaf5df78e83ba4f6f92796b
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-21 13:52:37 +0200

    creo la struttura dentro la view dell'oggetto Mail NewPostAdminEmail

---
+++ resources/views/emails/new-post-admin-email.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Ciao Amministratore, è appena stato creato un nuovo post</h1>
+</body>
+</html>
