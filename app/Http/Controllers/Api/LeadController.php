229428bfbecd34956b27b41b3589afd1f493ad7f
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-21 15:47:26 +0200

    assegno a $new_lead una nuova istanza di Lead

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+// importo il model
+use App\Lead;
 
 class LeadController extends Controller
 {   
     // inserisco una funzione store() all'interno del controller, il nome è dato dal fatto che salva una nuova riga nel database
-    public function store(){
-        dd('crea nuovo leed');
+    // per leggere i dati provenienti dal form inserisco Request $request in store(), dato che la chiamata è eseguita con metodo post posso vedere i risultati solo tramite postman
+    public function store(Request $request){
+    //    richiamo tutti i dati
+        $data = $request->all();
+        // dd($data);
+        // assegno a $new_lead una nuova istanza di Lead
+        $new_lead = new Lead();
+
     }
 
 }
