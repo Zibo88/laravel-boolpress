f99b22c32ad23da9f041854ab53e0aa5fec6435a
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-21 13:53:31 +0200

    associo la view all'oggetto Mail

--- app/Mail/NewPostAdminEmail.php
+++ app/Mail/NewPostAdminEmail.php
@@ -28,6 +28,6 @@ class NewPostAdminEmail extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('emails.new-post-admin-email');
     }
 }
