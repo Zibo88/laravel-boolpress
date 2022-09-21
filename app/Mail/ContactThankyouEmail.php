e614314813b4671c5a71008c0168061fd5f33fb7
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-21 18:51:24 +0200

    collego la mail alla view

--- app/Mail/ContactThankyouEmail.php
+++ app/Mail/ContactThankyouEmail.php
@@ -28,6 +28,6 @@ class ContactThankyouEmail extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('emails.contact-thankyou-email.blade.php');
     }
 }
