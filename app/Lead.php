80a8db028724ca0fe0a42e9bc638b757e57fc764
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-21 15:30:25 +0200

    assegno alle colonne il fillable per permettere poi il mass-assignment

--- app/Lead.php
+++ app/Lead.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lead extends Model
 {
-    //
+    // assegno alle colonne il fillable per permettere poi il mass-assignment
+    protected $fillable = [
+        'name',
+        'email',
+        'message',
+    ];
 }
